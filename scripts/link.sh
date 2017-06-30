828df74b88c623c777bc33affd00e0ffc07faafa
Pontus Persson <pontus.alexander@gmail.com>
2017-06-30 07:08:27 +0200

    Decouple link script.

---
+++ scripts/link.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+# Ensures all repos are running the same instance of dependencies.
+# Critical for type comparison, instance checking, and mocking.
+cd ./node_modules/three
+    yarn unlink
+    yarn link
+    cd -
+
+cd ./repos/testing
+    yarn unlink
+    yarn link
+    yarn link three
+    cd -
+
+cd ./repos/engine
+    yarn unlink
+    yarn link
+    yarn link three
+    yarn link @snakesilk/testing
+    cd -
+
+cd ./repos/xml-loader
+    yarn unlink
+    yarn link
+    yarn link three
+    yarn link @snakesilk/testing
+    yarn link @snakesilk/engine
+    cd -
+
+cd ./repos/megaman-kit
+    yarn unlink
+    yarn link
+    yarn link @snakesilk/engine
+    yarn link @snakesilk/xml-loader
+    cd -
+
+cd ./repos/megamanjs
+    yarn link @snakesilk/engine
+    yarn link @snakesilk/xml-loader
+    yarn link @snakesilk/megaman-kit
